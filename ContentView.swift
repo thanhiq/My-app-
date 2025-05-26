85adbf0e1c73d897a0e3ddd61e87a29a798e096c
thanhiq <tranminhthanh.bi2009@gmail.com>
2025-05-26 18:25:05 +0700

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,17 @@
 import SwiftUI
+import WebKit
 
-struct ContentView: View {
-    var body: some View {
-        WebView()
-            .edgesIgnoringSafeArea(.all)
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        config.preferences.javaScriptEnabled = true // cảnh báo deprecated, có thể sửa sau
+        return WKWebView(frame: .zero, configuration: config)
     }
-}
 
-#Preview {
-    ContentView()
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
 }
